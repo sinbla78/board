ad13a93124210ba15be4ae94a76e9e790445ac9e
sinbla78 <ay612@dsm.hs.kr>
2023-06-15 17:10:58 +0900

    #8 - 도메인 클래스 설계{게시글, 댓글}
    도메인 정보 설계
    기초적인 게시글과 댓글 표현이 가능하도록 함
    DB와 연동을 추가적인 표현을 추후에 업데이트 할 예정

--- fastcampus-project-board/src/main/java/com/fastcampus/fastcampusprojectboard/domain/Article.java
+++ fastcampus-project-board/src/main/java/com/fastcampus/fastcampusprojectboard/domain/Article.java
@@ -8,9 +8,9 @@ public class Article {
     private String content; // 본문
     private String hashtag; // 해시태그
 
-    private LocalDateTime createdAt; // 생성일시
+    private LocalDateTime createdAt; // 생성 일시
     private String createdBy; // 생성자
-    private LocalDateTime modifiedAt; // 수정일시
+    private LocalDateTime modifiedAt; // 수정 일시
     private String modifiedBy; // 수정자
 
 }
