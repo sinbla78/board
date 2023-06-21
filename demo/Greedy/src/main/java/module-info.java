f5349e5225fbb4c132abc0d49860c300083fff1b
sinbla78 <ay612@dsm.hs.kr>
2023-06-21 18:13:33 +0900

    수정

--- demo/Greedy/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.greedy {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.greedy to javafx.fxml;
-    exports com.example.greedy;
-}
--- demo/demo2/src/main/java/com/example/demo/enumpkg/ServiceResult.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.enumpkg;
-
-
-public enum ServiceResult {
-    OK, FAIL, NOTEXIST, HASCHILD
-}
