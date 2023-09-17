2917c0fec9271161e7ccbec553be4868fb147ee2
sinbla78 <sinbla78@gmail.com>
2023-09-18 08:28:34 +0900

    :memo: file delete

--- Pratice/Socket/src/main/java/com/example/socket/domain/auth/RefreshTokenRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.socket.domain.auth;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken,String> {
-
-}
